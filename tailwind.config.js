b314f6ff1b11f761764799ab450acee6d620f946
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-02-13 20:45:09 +0530

    Added product page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure Tailwind scans all your React components
+  ],
+  theme: {
+    extend: {}, // Customize theme here (colors, spacing, etc.)
+  },
+  plugins: [],
+};
